2142de80c60ccb8693d60d5eee9db755e532783e
n3rd3x3 <me@n3rd3x3.xyz>
2022-05-26 12:15:47 -0500

    ⬆ v1.0.1 (fix some credits)

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.n3rd3x3.popWALLS"
-    const val version = 100
-    const val versionName = "1.0.0"
+    const val version = 101
+    const val versionName = "1.0.1"
 }
