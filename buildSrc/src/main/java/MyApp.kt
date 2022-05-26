2dd7ae38f9da3a71968f6d1c9c9309b31c7d4527
n3rd3x3 <me@n3rd3x3.xyz>
2022-05-26 00:10:00 -0500

    change icon, various tiny tweaks, v0.9.2

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.n3rd3x3.popWALLS"
-    const val version = 91
-    const val versionName = "0.9.1"
+    const val version = 92
+    const val versionName = "0.9.2"
 }
