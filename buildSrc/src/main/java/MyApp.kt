6dbdc551accf59617b82c14f316a80df0aac2f20
n3rd3x3 <me@n3rd3x3.xyz>
2022-05-25 23:29:03 -0500

    update credits, change to v0.9.1

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.n3rd3x3.popWALLS"
-    const val version = 1
-    const val versionName = "0.0.1"
+    const val version = 91
+    const val versionName = "0.9.1"
 }
