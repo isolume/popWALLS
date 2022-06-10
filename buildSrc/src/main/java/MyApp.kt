13dcbb1a3968d8f04a1ed7749038d09c146e0e29
nerd <nerd@n3rd3x3.dev>
2022-06-09 21:33:21 -0500

    v1.1.0
    add more walls, dynamic icon, and more

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.n3rd3x3.popWALLS"
-    const val version = 101
-    const val versionName = "1.0.1"
+    const val version = 110
+    const val versionName = "1.1.0"
 }
